6467c0bc622262148fc015c70c32da47678b9aa8
APX <aphamx.mail+1@gmail.com>
2015-08-03 01:55:01 -0500

    Java Test
    Receive information from web browser and print to server console.

---
+++ src/Something/DatabaseCrud.java
@@ -0,0 +1,14 @@
+package Something;
+
+public class DatabaseCrud {
+	
+	
+
+		public static void insertdb(String Input1){		//This creates a function. Thanks Matt Ly!
+			System.out.println(Input1);
+			
+			
+		
+		
+	}
+}
---
+++ src/Something/SendtoServer.java
@@ -0,0 +1,47 @@
+package Something;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SendtoServer
+ */
+@WebServlet(description = "Sends data to server", urlPatterns = { "/SendtoServer" })
+public class SendtoServer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public SendtoServer() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		String Input1 = request.getParameter("Input1");
+		DatabaseCrud.insertdb((Input1));
+		System.out.println(request.getParameter("TestRadio"));
+		System.out.println(request.getParameter("Input1"));
+		System.out.println(request.getParameter("Input2"));
+		
+	}
+
+}
